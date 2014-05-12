f03ec1d6613c936895e7104031951190dc804d26
Rohan Sahai <rsahai91@gmail.com>
2014-05-12 14:38:25 -0700

    client photo class intial methods

--- PhotoTagger-master/app/assets/javascripts/application.js
+++ PhotoTagger-master/app/assets/javascripts/application.js
@@ -23,7 +23,50 @@
 //= require_tree .
 (function(root){
   var Tagger = root.Tagger = (root.Tagger || {})
-  var Photo = Tagger.photo = function(attrs) {
+  var Photo = Tagger.Photo = function(attrs) {
     this.attributes = attrs;
   }
+
+  _.extend(Photo.prototype, {
+    get: function(attr_name){
+      return this.attributes[attr_name];
+    },
+    set: function(attr_name, value){
+      this.attributes[attr_name] = value;
+    },
+    create: function(callback){
+      var that = this;
+      if (!this.attributes.id){
+        $.ajax({
+          url: 'api/photos',
+          method: 'POST',
+          data: {
+            photo: this.attributes
+          },
+          success: function(response){
+            _.extend(that.attributes, response);
+            callback(response)
+          }
+        });
+      }
+    },
+    save: function(callback){
+      if (this.attributes.id){
+        $.ajax({
+          url: 'api/photos/' + this.attributes.id,
+          method: 'PATCH', //might need to change this?!?
+          data: {
+            photo: this.attributes
+          },
+          success: function(response){
+            _.extend(this.attributes, response);
+            callback(response);
+          }
+        });
+      } else {
+        this.create(callback);
+      }
+    }
+
+  });
 })(this)
